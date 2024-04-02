c13e759a723f18a6bfc0f459879668fb5697018c
SwiftSoup3 <carl.aslund@edu.vellinge.se>
2024-04-02 10:23:41 +0200

    nu kraschar den inte om man går in på en sida som inte finns

--- app.js
+++ app.js
@@ -4,15 +4,16 @@ var url = require('url')
 var path = require('path')
 const port = 3000
 
-
 const server = http.createServer(function(req, res){
   var urlname = url.parse(req.url, true) 
   var filename = 'index.html'
   
+  //kollar om vi är på en sida
   if (urlname.pathname != '/'){
     filename = urlname.pathname.slice(1)
   }
 
+  //laddar in externa filer (css & js)
   var ext = path.extname(url.parse(req.url).pathname)
   if (ext){
     if (ext === '.css'){
@@ -21,26 +22,29 @@ const server = http.createServer(function(req, res){
       res.writeHead(200, {'Content-Type': 'text/javascript'})
     }
 
-    res.write(fs.readFileSync(__dirname + url.parse(req.url).pathname))
-  } else {
-    res.writeHead(200, {'Content-Type': 'text/html'})
-    res.write(fs.readFileSync(filename))
-  }
-  res.end()
-  /*
-  fs.readFile(filename, function(err, data) {
-        if (err) {
-            res.writeHead(404)
-            res.write('Error: File Not Found')
-        } else {
-            
-            res.writeHead(200, {'Content-Type': 'text/html'})
-            res.write(data)
-        }
-        
-        res.end()
+    fs.readFile(__dirname + url.parse(req.url).pathname, function(err, data) {
+      if (err){
+        res.write("error")
+      } else {
+        res.write(data)
+      }
     })
-    */
+  } else {
+    //laddar in html-filen
+    fs.readFile(filename, function(err, data) {
+      if (err) {
+          res.writeHead(404)
+          res.write('Error: File Not Found')
+      } else {
+          
+          res.writeHead(200, {'Content-Type': 'text/html'})
+          res.write(data)
+      }
+      
+      res.end()
+  })
+  }
+
 })
 
 server.listen(port, (error) => {
