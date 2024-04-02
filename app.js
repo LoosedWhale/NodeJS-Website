33415e7c0a0d39d020c855764287f5514913a163
SwiftSoup3 <carl.aslund@edu.vellinge.se>
2024-04-02 09:37:18 +0200

    kan nu gå mellan html dokument via href

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
 const http = require('http')
 const fs = require('fs')
+var url = require('url')
 const port = 3000
 
+
 const server = http.createServer(function(req, res){
-    fs.readFile('index.html', function(err, data) {
+  var path = url.parse(req.url, true) 
+  var filename = 'index.html'
+  
+  if (path.pathname != '/'){
+    filename = path.pathname.slice(1)
+  }
+  
+  
+  console.log(filename)
+  fs.readFile(filename, function(err, data) {
         if (err) {
             res.writeHead(404)
             res.write('Error: File Not Found')
         } else {
+            
             res.writeHead(200, {'Content-Type': 'text/html'})
             res.write(data)
         }
+        
         res.end()
     })
 })
